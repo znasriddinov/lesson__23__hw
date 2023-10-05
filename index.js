cda7691eaacf713619ee00ab6f8b06751634ee93
znasriddinov <143064598+znasriddinov@users.noreply.github.com>
2023-10-05 20:51:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,154 @@
+// Begin 1
+
+/* const a = parseInt(prompt("a ni kiriting"));
+
+// const P = a * 4;
+
+alert(P);
+*/
+
+// Begin 2
+
+/*
+const a = parseInt(prompt("a ni kiriting"));
+
+const S = a * 2;
+
+alert(S); 
+*/
+
+// Begin 3
+
+/*
+
+const a = parseInt(prompt("a ni kiriting"));
+const b = parseInt(prompt("b ni kiriting"));
+
+const S = a * b;
+const P = 2 * (a + b);
+
+alert(S);
+alert(P);
+*/
+
+// Begin 4
+
+/*
+const d = parseInt(prompt("d ni kiriting"));
+const pi = 3.14;
+
+const L = pi * d;
+
+alert(L);
+*/
+
+// Begin 5
+
+/*
+const a = parseInt(prompt("a ni kriting"));
+const V = a * a * a;
+const S = 6 * (a * a);
+
+alert(V);
+alert(S);
+*/
+
+// Begin 6
+
+/*
+const a = parseInt(prompt("a ni kriting"));
+const b = parseInt(prompt("b ni kriting"));
+const c = parseInt(prompt("c ni kriting"));
+
+const V = a * b * c;
+const S = 2 * (a * b + b * c + a * c);
+
+alert(V);
+alert(S);
+*/
+
+// Begin 7
+
+/*
+const R = parseInt(prompt("R ni kriting"));
+
+const pi = 3.14;
+
+const L = 2 * pi * R;
+const S = pi * (R * R);
+
+alert(L);
+alert(S);
+*/
+
+// Begin 8
+
+/*
+const a = parseInt(prompt("a ni kiriting"));
+const b = parseInt(prompt("b ni kiriting"));
+
+const A = (a + b) / 2;
+
+alert(A);
+*/
+
+// Integer 19
+
+/*
+const N = parseInt(prompt("Sekundni kiriting"));
+
+const T = N / 60;
+
+alert(T);
+*/
+
+// Integer 20
+
+/*
+const N = parseInt(prompt("Sekundni kiriting"));
+
+const T = N / 3600;
+
+alert(T);
+*/
+
+// Integer 1
+
+/*
+const L = parseInt(prompt("Santimetrni kiriting"));
+
+const m = L / 100;
+
+alert(m);
+*/
+
+// Integer 2
+
+/*
+const M = parseInt(prompt("kilogramm ni kiriting"));
+
+const t = M / 1000;
+
+alert(t);
+*/
+
+// Integer 3
+
+/*
+const bayt = parseInt(prompt("bayt ni kiriting"));
+
+const kb = bayt / 1024;
+
+alert(kb);
+*/
+
+// Integer 4
+
+/*
+const a = parseInt(prompt("Kotta sonni kiriting"));
+const b = parseInt(prompt("Kichkina sonni kiriting"));
+
+const equals = a / b;
+
+alert(equals);
+*/
